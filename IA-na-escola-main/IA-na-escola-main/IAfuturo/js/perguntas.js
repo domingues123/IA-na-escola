0862e7336189d6ec28876b0d2d4d4ce1c1c83604
domingueira <137198162+domingues123@users.noreply.github.com>
2025-08-15 11:37:07 -0300

    Add files via upload

---
+++ IA-na-escola-main/IA-na-escola-main/IAfuturo/js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "2 xicras de de canjica de milho",
+                afirmacao: [
+                    "No início ficou com medo do que essa canjica poderia ficar ruim.",
+                    "Achou assustador pensar na velocidade na qual as  pessoas come canjica"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "oleo de motor",
+                afirmacao: [
+                    "voçê deve estar enlouquecendo",
+                    "Pensou que oleo pode ser bom na sua vida."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "2 litros de agua",
+                afirmacao: [
+                    "dai Conseguiu utilizar o ultimo litro de agua",
+                    "isso ai",
+                    "dois litros de agua é o suficiente"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "grama contaminada",
+                afirmacao: [
+                    "você é louco?",
+                    "voê ja comeu isso?",
+                    "que?"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "DOCE DE LEITE",
+                afirmacao: [
+                    "VOCE É HUMANO, PARABENS",
+                    ".uauuuuuuuuu",
+                    ".vc é a melhor pessoa"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "PARAFUSOS",
+                afirmacao: [
+                    "LOUCO",
+                    "LÉLÉ",
+                    "JA DISSE QUE VC É MALUCO?."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "1 xicara de acucar",
+                afirmacao: [
+                    ". acucar para ficar bem docinho",
+                    "sem acucar é horrivel"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "pregos enferrujados",
+                afirmacao: [
+                    "que canjica é essa?",
+                    "quer morrer?"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "coco ralado",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "pessoas.",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "1 litro de leite",
+                afirmacao: [
+                    ". leite é bom, ainda mais quando é denso",
+                    "o de saquinho eu recomendo"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "sapato velho",
+                afirmacao: [
+                    "mano?",
+                    "voce ta pirado"
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "canela",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "papel ensaguentando",
+                afirmacao: [
+                    "",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "leite condensado e creme de leite",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda fazer uma canjiquinha tradicional ",
+                    "Ainda acha que os meios de fazer canjica tradicional são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer canjica."
+                ],
+            },
+            {
+                texto: "geladeira da sansung.",
+                afirmacao: [
+                    "...voce é uma pessoa?",
+                    "você é vivo?",
+                    "..."
+                ],
+            },
+        ]
+    }
+];
--- IA-na-escola-main/IAfuturo/js/perguntas.js
+++ IA-na-escola-main/IAfuturo/js/perguntas.js
@@ -1,43 +1,43 @@
 export const perguntas = [
     {
-        enunciado: "",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "2 xicras de de canjica de milho",
+                texto: "Isso é assustador!",
                 afirmacao: [
-                    "No início ficou com medo do que essa canjica poderia ficar ruim.",
-                    "Achou assustador pensar na velocidade na qual as  pessoas come canjica"
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
                 ],
                 proxima: 1,
             },
             {
-                texto: "oleo de motor",
+                texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "voçê deve estar enlouquecendo",
-                    "Pensou que oleo pode ser bom na sua vida."
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo, e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "2 litros de agua",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "dai Conseguiu utilizar o ultimo litro de agua",
-                    "isso ai",
-                    "dois litros de agua é o suficiente"
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
                 ],
                 proxima: 3,
             },
             {
-                texto: "grama contaminada",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    "você é louco?",
-                    "voê ja comeu isso?",
-                    "que?"
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
                 ],
                 proxima: 4,
             },
@@ -67,21 +67,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
         alternativas: [
             {
-                texto: "1 xicara de acucar",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    ". acucar para ficar bem docinho",
-                    "sem acucar é horrivel"
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 5,
             },
             {
-                texto: "pregos enferrujados",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "que canjica é essa?",
-                    "quer morrer?"
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 6,
             },
@@ -109,21 +109,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "1 litro de leite",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    ". leite é bom, ainda mais quando é denso",
-                    "o de saquinho eu recomendo"
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 7,
             },
             {
-                texto: "sapato velho",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "mano?",
-                    "voce ta pirado"
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 7,
             },
@@ -151,21 +151,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "leite condensado e creme de leite",
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
                 afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda fazer uma canjiquinha tradicional ",
-                    "Ainda acha que os meios de fazer canjica tradicional são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer canjica."
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
                 ],
             },
             {
-                texto: "geladeira da sansung.",
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
                 afirmacao: [
-                    "...voce é uma pessoa?",
-                    "você é vivo?",
-                    "..."
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
                 ],
             },
         ]
